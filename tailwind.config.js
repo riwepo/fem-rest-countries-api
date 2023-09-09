517660da58be260707618ac9590711890bd186c1
Richard Potter <richardpotter.au@gmail.com>
2023-09-10 08:25:39 +1000

    set up font weights

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,14 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    fontFamily: {
+      nunito: ["nunito", "sans-serif"],
+    },
+    fontWeight: {
+      normal: "300",
+      semibold: "600",
+      bold: "800",
+    },
     extend: {},
   },
   plugins: [],
