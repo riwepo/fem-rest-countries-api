71ca21747b7d93cf527cbb4bcff8b1413a1a5ed2
Richard Potter <richardpotter.au@gmail.com>
2023-09-11 12:31:52 +1000

    disply list

--- src/components/DesignSystem.tsx
+++ src/components/DesignSystem.tsx
@@ -1,18 +1,29 @@
 // import React from ".react";
-import CountrySummary from "./CountrySummary";
+// import CountrySummary from "./CountrySummary";
+import CountrySummaryList from "./CountrySummaryList";
+import ICountrySummary from "./ICountrySummary";
 
-function DesignSystem() {
+const DesignSystem: React.FC = () => {
+  const australia: ICountrySummary = {
+    name: "Australia",
+    population: 666,
+    region: "Oceanina",
+    capital: "Canberra",
+    flagUrl: "https://flagcdn.com/au.svg",
+  };
+  const countries: ICountrySummary[] = [
+    australia,
+    australia,
+    australia,
+    australia,
+    australia,
+  ];
   return (
     <div>
-      <CountrySummary
-        name="Australia"
-        population={25000000}
-        region="Oceania"
-        capital="Canberra"
-        flagUrl="https://flagcdn.com/au.svg"
-      />
+      {/* <CountrySummary country={australia} /> */}
+      <CountrySummaryList countries={countries} />
     </div>
   );
-}
+};
 
 export default DesignSystem;
