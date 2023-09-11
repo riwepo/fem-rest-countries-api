4e454ad2aa170695031867a82e4a3530f85bfd61
Richard Potter <richardpotter.au@gmail.com>
2023-09-11 12:31:29 +1000

    use interface

--- src/components/CountrySummary.tsx
+++ src/components/CountrySummary.tsx
@@ -1,35 +1,36 @@
 import Card from "./Card";
 
-function CountrySummary(props: {
-  name: string;
-  population: number;
-  region: string;
-  capital: string;
-  flagUrl: string;
-}) {
+import ICountrySummary from "./ICountrySummary";
+
+interface CountrySummaryProps {
+  country: ICountrySummary;
+}
+
+const CountrySummary: React.FC<CountrySummaryProps> = (props) => {
   return (
     <Card className="m-10 grid h-[24rem] w-[20rem] grid-rows-[1fr_1fr]">
       <img
-        src={props.flagUrl}
+        src={props.country.flagUrl}
         alt="flag"
         className="h-[12rem] w-full object-cover"
       />
       <div className="flex flex-col justify-center gap-3 p-2 ">
-        <p className="text-lg font-bold ">{props.name}</p>
+        <p className="text-lg font-bold ">{props.country.name}</p>
         <div>
           <p className="text-sm font-normal">
-            <span className="font-bold">Population:</span> {props.population}
+            <span className="font-bold">Population:</span>{" "}
+            {props.country.population}
           </p>
           <p className="text-sm font-normal">
-            <span className="font-bold">Region:</span> {props.region}
+            <span className="font-bold">Region:</span> {props.country.region}
           </p>
           <p className="text-sm font-normal">
-            <span className="font-bold">Capital:</span> {props.capital}
+            <span className="font-bold">Capital:</span> {props.country.capital}
           </p>
         </div>
       </div>
     </Card>
   );
-}
+};
 
 export default CountrySummary;
