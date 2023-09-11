3ec6b18af3481fd40045309d23dfccb4a2a97c9c
Richard Potter <richardpotter.au@gmail.com>
2023-09-11 12:32:38 +1000

    use interface

--- src/tests/CountrySummary.test.tsx
+++ src/tests/CountrySummary.test.tsx
@@ -1,20 +1,23 @@
 // import React from "react";
 import { describe, test, expect } from "vitest";
 import { render, screen } from "@testing-library/react";
+import "@testing-library/jest-dom";
 
 import CountrySummary from "../components/CountrySummary";
+import ICountrySummary from "../components/ICountrySummary";
 
 describe("country summary test suite", () => {
+  const australia: ICountrySummary = {
+    name: "Australia",
+    population: 666,
+    region: "Oceanina",
+    capital: "Canberra",
+    flagUrl: "https://flagcdn.com/au.svg",
+  };
   test("renders img and alt", () => {
-    render(
-      <CountrySummary
-        name="Australia"
-        population={666}
-        region="Oceanina"
-        capital="Canberra"
-        flagUrl='"https://flagcdn.com/au.svg"'
-      />,
-    );
+    render(<CountrySummary country={australia} />);
+    const imgElement = screen.getByRole("img");
+    expect(imgElement).toBeInTheDocument();
     const nameElement = screen.getByText("Australia");
     expect(nameElement).toBeInTheDocument();
     const populationLabelElement = screen.getByText("Population:");
