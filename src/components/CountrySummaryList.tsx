b1075103333310d5dbb58f33a2c5f90cd084b3e9
Richard Potter <richardpotter.au@gmail.com>
2023-09-13 08:35:47 +1000

    call rest and put in state

--- src/components/CountrySummaryList.tsx
+++ src/components/CountrySummaryList.tsx
@@ -1,14 +1,31 @@
+import { useEffect, useState } from "react";
+
 import CountrySummary from "./CountrySummary";
-import ICountrySummary from "./ICountrySummary";
 
-interface CountrySummaryListProps {
-  countries: ICountrySummary[];
-}
+import { ICountrySummary } from "../country_api/get-countries-helpers";
+import { getAllCountriesSummary } from "../country_api/get-countries";
 
-const CountrySummaryList: React.FC<CountrySummaryListProps> = (props) => {
+// interface CountrySummaryListProps {
+//   countries: ICountrySummary[];
+// }
+
+// const CountrySummaryList: React.FC<CountrySummaryListProps> = (props) => {
+const CountrySummaryList: React.FC = () => {
+  const [allCountriesSummary, setAllCountriesSummary] = useState<
+    ICountrySummary[]
+  >([]);
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await getAllCountriesSummary();
+      if (result.isOk) {
+        setAllCountriesSummary(result.value as ICountrySummary[]);
+      }
+    };
+    fetchData();
+  }, []);
   return (
     <div className="grid grid-cols-4">
-      {props.countries.map((country) => {
+      {allCountriesSummary.map((country) => {
         return <CountrySummary key={country.name} country={country} />;
       })}
     </div>
