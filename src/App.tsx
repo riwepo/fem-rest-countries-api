53b8b06db2890032894f4396d2bb891044e59670
Richard Potter <richardpotter.au@gmail.com>
2023-09-14 08:15:34 +1000

    put all features into app

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,29 @@
-import DesignSystem from "./components/DesignSystem";
+import CountrySummaryList from "./components/CountrySummaryList";
+// import DesignSystem from "./components/DesignSystem";
+import FilterCombo from "./components/FilterCombo";
+import Header from "./components/Header";
+import SearchInput from "./components/SearchInput";
+
+const filtercomboOptions = ["option1", "option2", "option3", "option4"];
 
 const App: React.FC = () => {
-  return <DesignSystem />;
+  // return <DesignSystem />;
+  const filterSelectionChangedHandler = (selectedRegion: string) => {
+    console.log(selectedRegion);
+  };
+  return (
+    <div>
+      <Header />
+      <div className="flex justify-between p-2">
+        <SearchInput />
+        <FilterCombo
+          options={filtercomboOptions}
+          onSelectionChanged={filterSelectionChangedHandler}
+        />
+      </div>
+      <CountrySummaryList />
+    </div>
+  );
 };
 
 export default App;
