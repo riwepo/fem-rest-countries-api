15e72d20acdddd4638c90413c8dd55eaa6734bd3
Richard Potter <richardpotter.au@gmail.com>
2023-09-13 15:24:56 +1000

    click tp show selection

--- src/components/FilterCombo.tsx
+++ src/components/FilterCombo.tsx
@@ -1,6 +1,9 @@
 import Card from "./Card";
 
+import { useState } from "react";
+
 import icons8ChevronDown from "../assets/icons8-chevron-down-24.png";
+import icons8ChevronUp from "../assets/icons8-chevron-up-24.png";
 
 interface FilterComboProps {
   options: string[];
@@ -8,17 +11,32 @@ interface FilterComboProps {
 }
 
 const FilterCombo: React.FC<FilterComboProps> = (props) => {
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+  const clickHandler = () => {
+    console.log("click");
+    setIsOpen((current: boolean) => !current);
+  };
   return (
     <Card className="relative inline-flex">
-      <button className="flex items-center justify-around gap-2 p-2">
+      <button
+        className="flex items-center justify-around gap-2 p-2"
+        onClick={clickHandler}
+      >
         <p className="p-1">Filter by Region</p>
-        <img src={icons8ChevronDown} alt="chevron down" className="w-8" />
+        {!isOpen && (
+          <img src={icons8ChevronDown} alt="chevron down" className="w-8" />
+        )}
+        {isOpen && (
+          <img src={icons8ChevronUp} alt="chevron up" className="w-8" />
+        )}
       </button>
-      <Card className="absolute bottom-0 left-0 flex translate-y-[105%] transform flex-col gap-2 p-2">
-        {props.options.map((option) => {
-          return <p>{option}</p>;
-        })}
-      </Card>
+      {isOpen && (
+        <Card className="absolute bottom-0 left-0 flex translate-y-[105%] transform flex-col gap-2 p-2">
+          {props.options.map((option) => {
+            return <p key={option}>{option}</p>;
+          })}
+        </Card>
+      )}
     </Card>
   );
 };
