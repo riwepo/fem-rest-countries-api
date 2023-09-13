127086323bc01edbe0d1e4534c97bdd9accda756
Richard Potter <richardpotter.au@gmail.com>
2023-09-14 08:36:32 +1000

    test suite getUniqueRegions

--- src/tests/get-countries.test.tsx
+++ src/tests/get-countries.test.tsx
@@ -4,14 +4,48 @@ import { describe, test, expect } from "vitest";
 import { IGetCountriesResult } from "../country_api/get-countries-helpers.ts";
 import {
   getAllCountriesSummary,
-  // getAllRegions,
+  getUniqueRegions,
 } from "../country_api/get-countries.ts";
 
-describe("get countries summary test suite", () => {
+describe("getAllCountriesSummary test suite", () => {
   test("get all countries summary", async () => {
     const result: IGetCountriesResult = await getAllCountriesSummary();
     expect(result.isOk).toBe(true);
     expect(result.value).not.toBeNull();
     expect(Array.isArray(result.value)).toBeTruthy();
+  });
+});
+
+describe("getUniqueRegions test suite", () => {
+  test("getUniqueRegions strips region field from object", () => {
+    const testData = [{ region: "region1" }];
+    const result = getUniqueRegions(testData);
+    expect(result).toEqual(["region1"]);
+  });
+
+  test("getUniqueRegions returns all values if they are unique", () => {
+    const testData = [
+      { region: "region1" },
+      { region: "region2" },
+      { region: "region3" },
+    ];
+    const result = getUniqueRegions(testData);
+    expect(result).toEqual(["region1", "region2", "region3"]);
+  });
+
+  test("getUniqueRegions strips duplicate values", () => {
+    const testData = [
+      { region: "region1" },
+      { region: "region1" },
+      { region: "region2" },
+      { region: "region2" },
+      { region: "region2" },
+      { region: "region3" },
+      { region: "region3" },
+      { region: "region3" },
+      { region: "region3" },
+    ];
+    const result = getUniqueRegions(testData);
+    expect(result).toEqual(["region1", "region2", "region3"]);
   });
 });
