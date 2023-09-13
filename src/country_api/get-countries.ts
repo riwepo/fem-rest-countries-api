2cc84f46676e8e2d8e3aed29407be646b61e7bb3
Richard Potter <richardpotter.au@gmail.com>
2023-09-14 08:35:47 +1000

    move getUniqueRegions here

--- src/country_api/get-countries.ts
+++ src/country_api/get-countries.ts
@@ -1,45 +1,11 @@
 import {
-  //getUniqueRegions,
   wrapInResultObject,
-  //IGetCountriesResult,
   processError,
   ICountrySummary,
   convertToCountrySummary,
   IGetCountriesResult,
   sortCountrySummary,
 } from "./get-countries-helpers";
-
-// sample URL to get all regions
-// https://restcountries.com/v3.1/all?fields=region
-
-// // calls restcounteies API and gets all regions
-// // returns value or error wrapped in an object
-// export async function getAllRegions(): Promise<IGetCountriesResult> {
-//   const url = new URL("https://restcountries.com/v3.1/all?fields=region");
-//   const result = getCountries(url);
-//   return result;
-// }
-
-// // calls restcountries API at given URL
-// // returns value or error wrapped in an object
-// export async function getCountries(url: URL): Promise<IGetCountriesResult> {
-//   let result;
-//   try {
-//     const response = await fetch(url);
-//     if (!response.ok) {
-//       throw new Error(
-//         `restcountries request failed with error: ${response.status}`,
-//       );
-//     }
-//     const getCountriesResult = await response.json();
-//     const uniqueRegions = getUniqueRegions(getCountriesResult);
-//     const result = wrapInResultObject(uniqueRegions);
-//     return result;
-//   } catch (error) {
-//     result = processError(error);
-//     return result;
-//   }
-// }q
 
 const GET_ALL_COUNTRIES_SUMMARY_URL = new URL(
   "https://restcountries.com/v3.1/all?fields=name,capital,region,population,flags",
@@ -66,4 +32,10 @@ export async function getAllCountriesSummary(): Promise<IGetCountriesResult> {
     result = processError(error);
     return result;
   }
+}
+
+export function getUniqueRegions(regionObjects: { region: string }[]) {
+  const regionStrings: string[] = regionObjects.map((object) => object.region);
+  const uniqueRegions = [...new Set(regionStrings)];
+  return uniqueRegions;
 }
