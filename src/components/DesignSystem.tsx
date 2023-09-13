091af5508f7002e5abc57aac34865c25c5bb7f49
Richard Potter <richardpotter.au@gmail.com>
2023-09-13 11:45:11 +1000

    display header

--- src/components/DesignSystem.tsx
+++ src/components/DesignSystem.tsx
@@ -1,27 +1,29 @@
 // import React from ".react";
 // import CountrySummary from "./CountrySummary";
-import CountrySummaryList from "./CountrySummaryList";
-import ICountrySummary from "./ICountrySummary";
+// import CountrySummaryList from "./CountrySummaryList";
+// import ICountrySummary from "./ICountrySummary";
+import Header from "./Header";
 
 const DesignSystem: React.FC = () => {
-  const australia: ICountrySummary = {
-    name: "Australia",
-    population: 666,
-    region: "Oceania",
-    capital: "Canberra",
-    flagUrl: "https://flagcdn.com/au.svg",
-  };
-  const countries: ICountrySummary[] = [
-    australia,
-    australia,
-    australia,
-    australia,
-    australia,
-  ];
+  // const australia: ICountrySummary = {
+  //   name: "Australia",
+  //   population: 666,
+  //   region: "Oceania",
+  //   capital: "Canberra",
+  //   flagUrl: "https://flagcdn.com/au.svg",
+  // };
+  // const countries: ICountrySummary[] = [
+  //   australia,
+  //   australia,
+  //   australia,
+  //   australia,
+  //   australia,
+  // ];
   return (
     <div>
       {/* <CountrySummary country={australia} /> */}
-      <CountrySummaryList countries={countries} />
+      {/* <CountrySummaryList/> */}
+      <Header />
     </div>
   );
 };
