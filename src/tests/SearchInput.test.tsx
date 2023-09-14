cc135827c4398f8c7ea1cb1267f7a70ea8c6067e
Richard Potter <richardpotter.au@gmail.com>
2023-09-14 14:16:10 +1000

    test search input change

--- src/tests/SearchInput.test.tsx
+++ src/tests/SearchInput.test.tsx
@@ -1,13 +1,16 @@
 // import React from "react";
-import { describe, test, expect } from "vitest";
-import { render, screen } from "@testing-library/react";
+import { describe, test, expect, vi } from "vitest";
+import { fireEvent, render, screen } from "@testing-library/react";
 import "@testing-library/jest-dom";
 
 import SearchInput from "../components/SearchInput";
 
 describe("SearchInput component test suite", () => {
+  const onSearchChanged = (search: string) => {
+    console.log(search);
+  };
   test("renders img and placeholder text", () => {
-    render(<SearchInput />);
+    render(<SearchInput onSearchChanged={onSearchChanged} />);
     const imgElement = screen.getByRole("img");
     expect(imgElement).toBeInTheDocument();
     const inputElement = screen.getByRole("textbox");
@@ -15,5 +18,21 @@ describe("SearchInput component test suite", () => {
     expect((inputElement as HTMLInputElement).placeholder).toBe(
       "Search for a country...",
     );
+  });
+  test("callback called on enter key", () => {
+    const enteredValue = "fred";
+    const onSearchChanged = vi.fn();
+    render(<SearchInput onSearchChanged={onSearchChanged} />);
+    const inputElement = screen.getByRole("textbox");
+    expect(inputElement).toBeInTheDocument();
+    fireEvent.change(inputElement, {
+      target: { value: enteredValue },
+    });
+    fireEvent.keyDown(inputElement, {
+      key: "Enter",
+      code: "Enter",
+      charCode: 13,
+    });
+    expect(onSearchChanged).toBeCalledWith(enteredValue);
   });
 });
