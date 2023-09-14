881abf20d4af86c4f8c1c8f6dae5de6b875a9d9a
Richard Potter <richardpotter.au@gmail.com>
2023-09-14 15:24:55 +1000

    got working again

--- src/components/DesignSystem.tsx
+++ src/components/DesignSystem.tsx
@@ -1,36 +1,40 @@
 // import React from ".react";
-// import CountrySummary from "./CountrySummary";
-// import CountrySummaryList from "./CountrySummaryList";
-// import ICountrySummary from "./ICountrySummary";
-// import Header from "./Header";
-// import SearchInput from "./SearchInput";
+import CountrySummary from "./CountrySummary";
+import CountrySummaryList from "./CountrySummaryList";
+import Header from "./Header";
+import SearchInput from "./SearchInput";
 import FilterCombo from "./FilterCombo";
 
+import { ICountrySummary } from "../helpers/interfaces";
+
 const DesignSystem: React.FC = () => {
-  // const australia: ICountrySummary = {
-  //   name: "Australia",
-  //   population: 666,
-  //   region: "Oceania",
-  //   capital: "Canberra",
-  //   flagUrl: "https://flagcdn.com/au.svg",
-  // };
-  // const countries: ICountrySummary[] = [
-  //   australia,
-  //   australia,
-  //   australia,
-  //   australia,
-  //   australia,
-  // ];
+  const australia: ICountrySummary = {
+    name: "Australia",
+    population: 666,
+    region: "Oceania",
+    capital: "Canberra",
+    flag: new URL("https://flagcdn.com/au.svg)",
+  };
+  const countries: ICountrySummary[] = [
+    australia,
+    australia,
+    australia,
+    australia,
+    australia,
+  ];
   const filtercomboOptions = ["option1", "option2", "option3", "option4"];
   const filterComboSelectionChangeHandler = (selection: string) => {
     console.log(selection);
   };
+  const searchChangeHandler = (search: string) => {
+    console.log(search);
+  };
   return (
     <div>
-      {/* <CountrySummary country={australia} /> */}
-      {/* <CountrySummaryList/> */}
-      {/* <Header /> */}
-      {/* <SearchInput /> */}
+      <CountrySummary country={australia} />
+      <CountrySummaryList countries={countries} />
+      <Header progressMessage="hello there" />
+      <SearchInput onSearchChanged={searchChangeHandler} />
       <FilterCombo
         options={filtercomboOptions}
         onSelectionChanged={filterComboSelectionChangeHandler}
