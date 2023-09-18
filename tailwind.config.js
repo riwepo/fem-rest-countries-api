775049952a9de7cfef144a03160acaf012c2376d
Richard Potter <richardpotter.au@gmail.com>
2023-09-18 15:44:09 +1000

    implement custom colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,6 +11,17 @@ export default {
       semibold: "600",
       bold: "800",
     },
+    colors: {
+      clrTransparent: "transparent",
+      clrCurrent: "currentColor",
+      clrBg: "hsl(0, 0%, 98%)", // very light gray
+      clrText: "hsl(200, 15%, 8%)", // very dark blue
+      clrElements: "hsl(0, 0%, 100%)", // light mode elements
+      clrInput: "hsl(0, 0%, 52%)", // dark gray
+      clrDarkBg: "hsl(207, 26%, 17%)", // very dark blue
+      clrDarkText: "hsl(0, 0%, 100%)", // white
+      clrDarkElements: "hsl(209, 23%, 22%)", // dark blue
+    },
     extend: {},
   },
   plugins: [],
